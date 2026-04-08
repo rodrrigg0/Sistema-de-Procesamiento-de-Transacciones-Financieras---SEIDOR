d907afce4aae3fbe3ceaae81600c6a24bd832d5d
rodrrigg0 <rodrigobachillersanz@gmail.com>
2026-04-08 16:29:45 +0200

    Entidad Transaccion

--- src/main/java/com/banco/transacciones/model/entity/Cliente.java
+++ src/main/java/com/banco/transacciones/model/entity/Cliente.java
@@ -5,13 +5,9 @@ import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
--- src/main/java/com/banco/transacciones/model/entity/Transaccion.java
+++ src/main/java/com/banco/transacciones/model/entity/Transaccion.java
@@ -1,5 +1,52 @@
 package com.banco.transacciones.model.entity;
 
-public class Transaccion {
+import com.banco.transacciones.model.enums.EstadoTransaccion;
+import com.banco.transacciones.model.enums.TipoTransaccion;
+import jakarta.persistence.*;
+import lombok.Data;
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.List;
 
+import org.hibernate.annotations.ValueGenerationType;
+
+@Data
+@Entity
+@Table(name = "transacciones")
+public class Transaccion {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "cuenta_origen", nullable = false, length = 34)
+	private String cuentaOrigen;
+	
+	@Column(name = "cuenta_origen", nullable = false, length = 34)
+	private String cuentaDestino;
+	
+	@Column(nullable = false, precision = 15, scale = 2)
+	private BigDecimal monto;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private EstadoTransaccion estado;
+	
+	@Column(name= "fecha_hora", nullable = false)
+	private Instant fechaHora;
+	
+	@Column(length = 255)
+	private String  descripcion;
+	
+	@Column(name = "riesgo_fraude")
+	private Double riesgoFraude;
+	
+	@OneToMany(mappedBy = "transaccion")
+	private List<AlertaFraude> alertas;
+	
+	@PrePersist
+    protected void onCreate() {
+        fechaHora = Instant.now();
+        if (riesgoFraude == null) riesgoFraude = 0.0;
+    }
 }
