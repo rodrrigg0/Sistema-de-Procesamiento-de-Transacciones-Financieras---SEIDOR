d0b91c418351330c274f0ca28222250ca9d9de1b
rodrrigg0 <rodrigobachillersanz@gmail.com>
2026-04-08 17:14:48 +0200

    ENTIDAD ALERTA FRAUDE

--- src/main/java/com/banco/transacciones/model/entity/AlertaFraude.java
+++ src/main/java/com/banco/transacciones/model/entity/AlertaFraude.java
@@ -1,5 +1,32 @@
 package com.banco.transacciones.model.entity;
 
-public class AlertaFraude {
+import com.banco.transacciones.model.enums.NivelRiesgo;
+import jakarta.persistence.*;
+import lombok.Data;
 
-}
+	@Data
+	@Entity
+	@Table(name = "alertas_fraude")
+	public class AlertaFraude {
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private Long id;
+		
+		@Column(name = "transaccion_id", nullable = false)
+		private Long transaccionId;
+		
+		@ManyToOne
+		@JoinColumn(name = "transaccion_id", insertable = false, updatable = false)
+		private Transaccion transaccion;
+		
+		@Enumerated(EnumType.STRING)
+	    @Column(nullable = false)
+	    private NivelRiesgo nivel;
+
+	    @Column(nullable = false)
+	    private String motivo;
+
+	    @Column(nullable = false)
+	    private Boolean revisada = false;
+	}
+
