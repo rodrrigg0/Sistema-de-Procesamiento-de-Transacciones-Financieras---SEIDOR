29f1b7d462b25cc7d11e92326b1acdaa7fd40ebe
rodrrigg0 <rodrigobachillersanz@gmail.com>
2026-04-08 12:44:45 +0200

    Creo el archivo en la pagina de spring y lo importo , una vez importado le modifico el .properties con mis credenciales de mysql y con su url correspondiente , inicio la maquona para ver si funciona correctamente .

---
+++ src/test/java/transacciones/TransaccionesApplicationTests.java
@@ -0,0 +1,13 @@
+package transacciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransaccionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
