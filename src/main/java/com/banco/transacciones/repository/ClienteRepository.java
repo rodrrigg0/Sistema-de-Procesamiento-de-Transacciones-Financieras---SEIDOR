4f4b4ed4e671026db12f9008ba0a492de26adaf7
rodrrigg0 <rodrigobachillersanz@gmail.com>
2026-04-09 16:06:17 +0200

    CUENTA INTERFACE

--- src/main/java/com/banco/transacciones/repository/ClienteRepository.java
+++ src/main/java/com/banco/transacciones/repository/ClienteRepository.java
@@ -3,9 +3,11 @@ package com.banco.transacciones.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.banco.transacciones.model.entity.Cliente;
 
+@Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long>{
 	Optional<Cliente> findByDni(String dni);
 	Optional<Cliente> findByEmail(String email);
---
+++ src/main/java/com/banco/transacciones/repository/CuentaRepository.java
@@ -0,0 +1,29 @@
+package com.banco.transacciones.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.banco.transacciones.model.entity.Cuenta;
+import com.banco.transacciones.model.enums.EstadoCuenta;
+
+import jakarta.persistence.LockModeType;
+
+@Repository
+public interface CuentaRepository extends JpaRepository<Cuenta, Long> {
+	
+	Optional<Cuenta>findByNumeroCuenta(String numeroCuenta);
+	
+	 @Lock(LockModeType.PESSIMISTIC_WRITE)
+	 @Query("SELECT c FROM Cuenta c WHERE c.numeroCuenta = :numeroCuenta")
+	 Optional<Cuenta> findByNumeroCuentaWithLock(@Param("numeroCuenta") String numeroCuenta);
+	
+	List<Cuenta> findByClienteId(Long clienteId);
+	List<Cuenta> findByEstado(EstadoCuenta estado);
+	boolean existsByNumeroCuenta(String numeroCuenta);
+}
